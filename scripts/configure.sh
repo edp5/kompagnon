da3edc2de3f95742c1d24e5f3e50289b84bbcbdf
theotime2005 <theotime.berthod@epitech.digital>
2025-09-12 15:11:40 +0200

    tech: migrate api to main folder

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -7,12 +7,10 @@ echo "Configuring the environment for the project..."
 # Install all dependencies
 echo "Installing dependencies..."
 npm ci || { echo "Error installing npm ci dependencies"; exit 1; }
-npm run ci:all || { echo "Error running npm run ci:all"; exit 1; }
 
-# api
 # Environment variable
 echo "Setting up environment variables..."
-cp ./api/sample.env ./api/.env || { echo "Error copying environment file"; exit 1; }
+cp ./sample.env ./.env || { echo "Error copying environment file"; exit 1; }
 
 # docker
 echo "Starting Docker"
@@ -20,12 +18,8 @@ docker compose up -d || { echo "Error starting Docker"; exit 1; }
 
 # database
 echo "Creating database..."
-cd ./api && npm run db:reset || { echo "Error resetting the database"; exit 1; }
+npm run db:reset || { echo "Error resetting the database"; exit 1; }
 
-# web
-# copy the sample env to env
-#echo "Setting up web environment variables..."
-#cp ./web/sample.env ./web/.env || { echo "Error copying web environment file"; exit 1; }
 
 # end
 echo "Configuration complete."
