65a8d436aa1b6ac07bc15b2f367c2c8d9301583b
theotime2005 <theotime.berthod@epitech.digital>
2025-09-19 10:55:42 +0200

    feat(web): display app only if api is ok

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,15 +1,27 @@
-<script setup></script>
+<script setup>
+import { onBeforeMount, ref } from "vue";
+
+import { apiCheck } from "@/adapters/api-check.js";
+
+const apiIsOk = ref(false);
+
+onBeforeMount(async () => {
+  apiIsOk.value = await apiCheck();
+});
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a
-      href="https://vuejs.org/"
-      target="_blank"
-      rel="noopener"
-    >vuejs.org</a> to read the
-    documentation
-  </p>
+  <div v-if="apiIsOk">
+    <h1>You did it!</h1>
+    <p>
+      Visit <a
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+      >vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
