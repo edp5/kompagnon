3aa1ce14552f9293aa41ed0f87687e53de7e41ef
KYRIE KANE <146706868+meardagoat@users.noreply.github.com>
2025-10-01 11:34:11 +0200

    tech(api): add userType column to users table
    - Add userType column with default value from USER_TYPES.USER constant
    - Column is not nullable for data integrity
    - Migration uses constants for consistency
    - Migration supports rollback with down function
    
    Part of #34

---
+++ api/db/migrations/20250930140000_add-userType-to-users.js
@@ -0,0 +1,26 @@
+import { USER_TYPES } from "../../src/shared/constants.js";
+
+const TABLE_NAME = "users";
+const COLUMN_NAME = "userType";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+async function up(knex) {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.string(COLUMN_NAME).defaultTo(USER_TYPES.USER).notNullable();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+async function down(knex) {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+}
+
+export { down, up };
