871776c456367105ec6ca44d9350ac4a534b0187
theotime2005 <theotime.berthod@epitech.digital>
2025-10-01 19:16:48 +0200

    feat(admin): setup the admin web app

---
+++ admin/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onBeforeMount, ref } from "vue";
+
+import { apiCheck } from "@/adapters/api-check.js";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+const apiIsOk = ref(false);
+
+onBeforeMount(async () => {
+  apiIsOk.value = await apiCheck();
+});
+</script>
+
+<template>
+  <div v-if="apiIsOk">
+    <h1>You did it!</h1>
+    <p>
+      Visit <a
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+      >vuejs.org</a> to read the
+      documentation
+    </p>
+    <FooterComponent />
+  </div>
+</template>
+
+<style scoped></style>
