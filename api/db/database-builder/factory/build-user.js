d806b9b60fdee6ae8b067d9f9341c397a1716b17
lgadrien <146069613+lgadrien@users.noreply.github.com>
2025-10-08 11:16:04 +0200

    tech(api): modified the databasebuilded

--- api/db/database-builder/factory/build-user.js
+++ api/db/database-builder/factory/build-user.js
@@ -1,7 +1,7 @@
 import { generatePassword } from "../../../src/identities-access-management/services/password-service.js";
 import { DEFAULT_USER_TYPE } from "../../../src/shared/constants.js";
 import { knex } from "../../knex-database-connection.js";
-async function buildUser({ firstname = "John", lastname = "Doe", email = "john.doe@example.net", birthday = "01/01/1970", created_at = new Date(), updated_at = new Date(), isActive = true, isChecked = true, hashedPassword = null, userType = DEFAULT_USER_TYPE } = {}) {
+async function buildUser({ firstname = "John", lastname = "Doe", email = "john.doe@example.net", birthday = "01/01/1970", created_at = new Date(), updated_at = new Date(), isActive = true, isChecked = true, hashedPassword = null, userType = DEFAULT_USER_TYPE, lastLoggedAt = null } = {}) {
   if (!hashedPassword) {
     hashedPassword = await generatePassword("password");
   }
@@ -16,6 +16,7 @@ async function buildUser({ firstname = "John", lastname = "Doe", email = "john.d
     isChecked,
     hashedPassword,
     userType,
+    lastLoggedAt,
   }).returning("*");
   return values;
 }
