c63b90d39b318469f35f9885284d47e866835d2c
theotime2005 <theotime.berthod@epitech.digital>
2025-10-31 09:34:24 +0100

    tech(api): update the vitest configuration

--- api/tests/setup.js
+++ api/tests/setup.js
@@ -2,24 +2,28 @@ import { afterAll, afterEach, beforeEach, vi } from "vitest";
 
 import { knex } from "../db/knex-database-connection.js";
 
-
 beforeEach(async () => {
-  await knex.raw("TRUNCATE TABLE " +
-        (await knex("pg_tables")
-          .select("tablename")
-          .where("schemaname", "public")
-          .pluck("tablename"))
-          .map((t) => `"${t}"`)
-          .join(", ") +
-        " RESTART IDENTITY CASCADE",
-  );
-});
+  const tables = await knex("pg_tables")
+    .select("tablename")
+    .where("schemaname", "public")
+    .pluck("tablename");
 
+  if (tables.length > 0) {
+    await knex.raw(
+      "TRUNCATE TABLE " +
+            tables.map((t) => `"${t}"`).join(", ") +
+            " RESTART IDENTITY CASCADE",
+    );
+  }
+});
 
 afterEach(function() {
   vi.restoreAllMocks();
+  vi.resetAllMocks();
+  vi.clearAllMocks();
 });
 
 afterAll(async () => {
   await knex.destroy();
 });
+
