dc87af09f2e577189b75beeb5f972ca9b7ce86a7
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-04-21 21:46:28 +0200

    test(web): add a fake localStorage

--- web/tests/setup.js
+++ web/tests/setup.js
@@ -1,5 +1,9 @@
 import { afterEach, vi } from "vitest";
 
+import localStorage from "./localStorage.js";
+
+global.localStorage = localStorage;
+
 afterEach(() => {
   vi.clearAllTimers();
   vi.clearAllMocks();
