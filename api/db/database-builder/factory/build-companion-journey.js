0a2e7b6df3a11bc26b644540664ec3238eeefa10
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-04-29 15:18:58 +0200

    tech(api): add some builders

---
+++ api/db/database-builder/factory/build-companion-journey.js
@@ -0,0 +1,31 @@
+import { knex } from "../../knex-database-connection.js";
+
+const TABLE_NAME = "companion_journeys";
+async function buildCompanionJourney({
+  userId,
+  departureTime = new Date("2021-01-01T00:00:00Z"),
+  arrivalTime = new Date("2021-01-01T01:00:00Z"),
+  departureAddress = "5 rue de la cantoche, 75015 Cherbour",
+  arrivalAddress = "30 chemin des faubours 7554 TOurnus",
+  departureLat = 0.0,
+  departureLon = 0.0,
+  arrivalLat = 0.0,
+  arrivalLon = 0.0,
+} = {}) {
+  const [values] = await knex(TABLE_NAME).insert({
+    userId,
+    departureTime,
+    arrivalTime,
+    departureAddress,
+    arrivalAddress,
+    departureLat,
+    departureLon,
+    arrivalLat,
+    arrivalLon,
+    created_at: new Date(),
+    updated_at: new Date(),
+  }).returning("*");
+  return values;
+}
+
+export { buildCompanionJourney };
--- api/db/database-builder/index.js
+++ api/db/database-builder/index.js
@@ -1,8 +1,14 @@
+import { buildCompanionJourney } from "./factory/build-companion-journey.js";
+import { buildFoundJourney } from "./factory/build-found-journey.js";
+import { buildPassengerJourney } from "./factory/build-passenger-journey.js";
 import { buildUser } from "./factory/build-user.js";
 
 const databaseBuilder = {
   factory: {
     buildUser,
+    buildPassengerJourney,
+    buildCompanionJourney,
+    buildFoundJourney,
   },
 };
 export default databaseBuilder;
