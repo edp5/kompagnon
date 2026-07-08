93e664ced7715429629b64a54f2cc94f8ed3ae7e
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-07-08 02:14:08 +0200

    refactor(api): use the usecase

--- api/src/identities-access-management/controllers/activate-user-controller.js
+++ api/src/identities-access-management/controllers/activate-user-controller.js
@@ -1,9 +1,8 @@
 import { celebrate, Joi, Segments } from "celebrate";
 
 import { logger } from "../../../logger.js";
-import ERRORS, { MESSAGE } from "../errors.js";
-import { activateUserById, findUserById } from "../repositories/user-repository.js";
 import { decodedToken } from "../services/token-service.js";
+import usecases from "../usecases/index.js";
 
 const activateUserSchema = celebrate({
   [Segments.HEADERS]: Joi.object({
@@ -16,8 +15,7 @@ const activateUserSchema = celebrate({
  * @param {object} req - Express request object
  * @param {object} res - Express response object
  * @param {Function} next - Express next middleware function
- * @param {Function} findUserByIdRepository - Function to find user by ID
- * @param {Function} activateUserByIdRepository - Function to activate user by ID
+ * @param {Function} activateUserUsecase - Function to activate user by ID
  * @param {Function} decodedTokenService - Function to decode JWT token
  * @returns {Promise<*>} - Express response
  */
@@ -25,8 +23,7 @@ async function activateUserController(
   req,
   res,
   next,
-  findUserByIdRepository = findUserById,
-  activateUserByIdRepository = activateUserById,
+  activateUserUsecase = usecases.activateUserUsecase,
   decodedTokenService = decodedToken,
 ) {
   try {
@@ -35,24 +32,12 @@ async function activateUserController(
     const decodedData = decodedTokenService(token);
     const userId = decodedData.userId;
 
-    const user = await findUserByIdRepository(userId);
+    await activateUserUsecase(userId);
 
-    if (!user) {
-      logger.warn(`Activation attempt for non-existent user ${userId}`);
-      return res.status(401).json({ error: ERRORS.USER.NOT_FOUND_OR_ALREADY_ACTIVE });
-    }
-
-    if (user.isActive) {
-      logger.info("Activation attempt for already active user", { userId });
-      return res.status(401).json({ error: ERRORS.USER.NOT_FOUND_OR_ALREADY_ACTIVE });
-    }
-
-    await activateUserByIdRepository(userId);
-
-    return res.status(201).json({ message: MESSAGE.USER_ACTIVATED_SUCCESSFULLY });
+    return res.status(201).send();
   } catch (error) {
     logger.error({ err: error }, "Error during user activation");
-    return res.status(500).json({ error: ERRORS.INTERNAL_SERVER_ERROR });
+    next(error);
   }
 }
 
