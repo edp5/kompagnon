0b50bb4657ce6a39849240719e64daaf2925b28a
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-08-04 10:37:38 +0200

    tech(api): add some constants

---
+++ api/src/journeys/constants.js
@@ -0,0 +1,8 @@
+const NOTIFY_LABEL = {
+  COMPANION_LABEL: "un accompagnateur",
+  PASSENGER_LABEL: "une personne à accompagner",
+};
+
+const MAIL_MATCH_SUBJECT = "Une correspondance de trajet a été trouvée sur Kompagnon";
+
+export { MAIL_MATCH_SUBJECT, NOTIFY_LABEL };
