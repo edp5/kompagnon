ad5a469caaa48525712fb01c7ea4d4be74e74cd1
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-08-09 19:11:53 +0200

    tech(api): update usecase to add phone number to database

--- api/src/identities-access-management/usecases/activate-user-usecase.js
+++ api/src/identities-access-management/usecases/activate-user-usecase.js
@@ -1,18 +1,26 @@
-import { UserIsAlreadyActive, UserNotFoundError } from "../errors.js";
-import { activateUserById, findUserById } from "../repositories/user-repository.js";
+import { PhoneNumberAlreadyUsedError, UserIsAlreadyActive, UserNotFoundError } from "../errors.js";
+import { activateUserById, findUserById, updateUserData } from "../repositories/user-repository.js";
 
 /**
  * Activates a user by their ID.
- * @param {number} userId - The ID of the user to activate.
- * @returns {Promise<void>} - A promise that resolves when the user is activated.
+ * @param {object} params - The parameters for activating a user.
+ * @param {number} params.userId - The ID of the user to activate.
+ * @param {string} params.phoneNumber - The phone number of the user to activate.
  */
-async function activateUserUsecase(userId) {
+async function activateUserUsecase({ userId, phoneNumber }) {
   const user = await findUserById(userId);
   if (!user) {
     throw new UserNotFoundError();
-  }
-  if (user.isActive) {
+  } else if (user.isActive) {
     throw new UserIsAlreadyActive();
+  }
+  try {
+    await updateUserData({ userId: userId, data: { phoneNumber } });
+  } catch (error) {
+    if (error.code === "23505") {
+      throw new PhoneNumberAlreadyUsedError();
+    }
+    throw error;
   }
   await activateUserById(userId);
 }
