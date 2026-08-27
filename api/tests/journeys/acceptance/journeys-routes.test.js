a5bf537999e0e6efc7031694e365f5e68c987194
theotime2005 <146077254+theotime2005@users.noreply.github.com>
2026-08-27 14:48:03 +0200

    test(api): check that phone number is present in data

--- api/tests/journeys/acceptance/journeys-routes.test.js
+++ api/tests/journeys/acceptance/journeys-routes.test.js
@@ -446,6 +446,31 @@ describe("Acceptance | Journeys | Journey routes", () => {
       expect(response.body.data[0].otherStatus).toBe(JOURNEY_STATUS.ACCEPTED);
     });
 
+    it("should return 200 and the matches of the owner when is accepted's journey", async () => {
+      // given
+      const passenger = await databaseBuilder.factory.buildUser({ role: USER_ROLE.INVALID });
+      const passengerJourney = await databaseBuilder.factory.buildPassengerJourney({ userId: passenger.id });
+      const companion = await databaseBuilder.factory.buildUser({ firstname: "Adrien", lastname: "Le Guen" });
+      const companionJourney = await databaseBuilder.factory.buildCompanionJourney({ userId: companion.id });
+      await databaseBuilder.factory.buildFoundJourney({
+        passengerJourneyId: passengerJourney.id,
+        companionJourneyId: companionJourney.id,
+        passengerStatus: JOURNEY_STATUS.ACCEPTED,
+        companionStatus: JOURNEY_STATUS.ACCEPTED,
+      });
+      const auth = generateAuthenticatedUser(passenger.id, passenger.userType);
+
+      // when
+      const response = await request(server).get(`/api/journeys/${passengerJourney.id}/matches`).set("Authorization", auth);
+
+      // then
+      expect(response.status).toBe(200);
+      expect(response.body.data).toHaveLength(1);
+      expect(response.body.data[0].user).toEqual({ firstname: "Adrien", lastname: "Le Guen", phoneNumber: companion.phoneNumber });
+      expect(response.body.data[0].myStatus).toBe(JOURNEY_STATUS.ACCEPTED);
+      expect(response.body.data[0].otherStatus).toBe(JOURNEY_STATUS.ACCEPTED);
+    });
+
     it("should return 404 when the journey is not the user's", async () => {
       // given
       const owner = await databaseBuilder.factory.buildUser({ role: USER_ROLE.INVALID });
