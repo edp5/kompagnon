53962a71310f3ccbd3fdc325820d2a7dd1149231
Adrien <146069613+lgadrien@users.noreply.github.com>
2026-08-28 11:05:45 +0200

    fix(api): return 204 No Content on PUT /journeys/found/:id
    The controller returned 201 Created for a PUT request that updates
    an existing resource. Per HTTP semantics, PUT should return 200 OK
    or 204 No Content. The Swagger documentation already declared 204,
    creating an inconsistency.
    
    Changed both status(201) calls to status(204) in the controller,
    updated JSDoc annotations, and aligned all unit and acceptance test
    expectations accordingly.
    
    The web frontend only checks response.ok (true for both 201 and
    204), so this is a non-breaking change.
    
    Closes #877

--- api/src/journeys/api/controllers/update-found-journey-status-controller.js
+++ api/src/journeys/api/controllers/update-found-journey-status-controller.js
@@ -18,14 +18,14 @@ const updateFoundJourneyStatusSchema = celebrate({
 /**
  * Controller to update the found journey status, it will check the user role and update the status accordingly
  * @param {object} req - The request object, it should contain the user id in the auth property, the found journey id in the params and the updated status in the body
- * @param {object} res - The response object, it will return a 201 status code if the status is updated successfully, otherwise it will return a 500 status code
+ * @param {object} res - The response object, it will return a 204 status code if the status is updated successfully, otherwise it will return a 500 status code
  * @param {Function} next - The next function to call in case of an error, it will pass the error to the error handling middleware
  * @param {Function} acceptCompanionUsecase - The usecase to accept the companion status, it will be injected for testing purposes, it should receive an object with the user id and the found journey id and return a promise
  * @param {Function} rejectCompanionUsecase - The usecase to reject the companion status, it will be injected for testing purposes, it should receive an object with the user id and the found journey id and return a promise
  * @param {Function} acceptPassengerUsecase - The usecase to accept the passenger status, it will be injected for testing purposes, it should receive an object with the user id and the found journey id and return a promise
  * @param {Function} rejectPassengerUsecase - The usecase to reject the passenger status, it will be injected for testing purposes, it should receive an object with the user id and the found journey id and return a promise
  * @param {Function} findUser - The function to find the user by id, it will be injected for testing purposes, it should receive the user id and return a promise that resolves to the user object
- * @returns {Promise<*>} - It will return a promise that resolves to the response object, it will return a 201 status code if the status is updated successfully, otherwise it will return a 500 status code
+ * @returns {Promise<*>} - It will return a promise that resolves to the response object, it will return a 204 status code if the status is updated successfully, otherwise it will return a 500 status code
  */
 async function updateFoundJourneyStatusController(
   req,
@@ -47,14 +47,14 @@ async function updateFoundJourneyStatusController(
       } else {
         await rejectPassengerUsecase({ userId: auth.userId, foundJourneyId: params.foundJourneyId });
       }
-      return res.status(201).send();
+      return res.status(204).send();
     case USER_ROLE.VALID:
       if (body.updatedStatus) {
         await acceptCompanionUsecase({ userId: auth.userId, foundJourneyId: params.foundJourneyId });
       } else {
         await rejectCompanionUsecase({ userId: auth.userId, foundJourneyId: params.foundJourneyId });
       }
-      return res.status(201).send();
+      return res.status(204).send();
     default:
       throw new UserHasNoRole();
     }
