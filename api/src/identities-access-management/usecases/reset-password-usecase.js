56140cef705fd92d6ef96e4bdbb977a0751b77fb
Theo <146077254+theotime2005@users.noreply.github.com>
2026-09-04 11:20:33 +0200

    Update api/src/identities-access-management/usecases/reset-password-usecase.js

--- api/src/identities-access-management/usecases/reset-password-usecase.js
+++ api/src/identities-access-management/usecases/reset-password-usecase.js
@@ -11,36 +11,28 @@ import { generatePassword } from "../services/password-service.js";
  * @param {object} params - Parameters
  * @param {string} params.token - The password reset token
  * @param {string} params.password - The new password
- * @param {Function} [params.findValidToken] - Function to lookup valid token
- * @param {Function} [params.markUsed] - Function to mark token as consumed
- * @param {Function} [params.hashPassword] - Function to hash the new password
- * @param {Function} [params.updatePassword] - Function to update user in DB
  * @returns {Promise<{ message: string }>} Resolves with status message
  */
 async function resetPasswordUsecase({
   token,
   password,
-  findValidToken = findValidPasswordResetToken,
-  markUsed = markPasswordResetTokenAsUsed,
-  hashPassword = generatePassword,
-  updatePassword = updateUserData,
 }) {
-  const resetToken = await findValidToken(token);
+  const resetToken = await findValidPasswordResetToken(token);
 
   if (!resetToken) {
     throw new InvalidOrExpiredPasswordResetTokenError();
   }
 
-  const hashedPassword = await hashPassword(password);
+  const hashedPassword = await generatePassword(password);
 
-  await updatePassword({
+  await updateUserData({
     userId: resetToken.userId,
     data: {
       hashedPassword,
     },
   });
 
-  await markUsed(resetToken.id);
+  await markPasswordResetTokenAsUsed(resetToken.id);
 
   return {
     message: "Password reset successful.",
